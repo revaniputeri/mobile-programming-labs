8a105f7ce1520c174ee97b5d3f6ae1a21caf7dee
Revani Nanda Putri <revaninandaputeri@gmail.com>
2025-10-10 14:55:56 +0700

    feat: adding tittle row

--- jobsheet-flutter-2/layout_flutter/lib/main.dart
+++ jobsheet-flutter-2/layout_flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'widgets/title_section.dart';
 
 void main() => runApp(const MyApp());
 
@@ -13,8 +14,10 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Column(
+          children: [
+            TitleSection.buildTitleSection(),
+          ],
         ),
       ),
     );
---
+++ jobsheet-flutter-2/layout_flutter/lib/widgets/title_section.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class TitleSection {
+  static Widget buildTitleSection() {
+    return Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /* soal 1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                 /* soal 2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Wisata Gunung di Batu',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Batu, Malang, Indonesia',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /* soal 3*/
+          Icon(
+            Icons.star,
+            color: Colors.red,
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
+  }
+}
